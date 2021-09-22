d7e63a48b5d477b109d47aa827e5aeb956fcbdc3
paidipremkumar <47975951+paidipremkumar@users.noreply.github.com>
2021-09-23 01:03:35 +0530

    Add files via upload

---
+++ Quizzler-iOS13/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by prem on 06/09/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text : String
+    let answer : String
+    init(q : String,a : String) {
+        text = q
+        answer = a
+    }
+}
